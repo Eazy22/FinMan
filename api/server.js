bb7def0526bda94f6bab28615b078d2bb80f64b2
Eazy 1.0 <effeyotahisrael@gmail.com>
2026-09-05 11:58:30 +0100

    server.js mongoose connection and setup

--- api/server.js
+++ api/server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import cors from "cors"
 import "dotenv/config";
+import mongoose from "mongoose";
 
 const app = express();
 
 const PORT = process.env.PORT;
 
+const MONGO_URI = process.env.MONGODB_URI;
+
+app.use(express.json())
 app.use(cors())
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running on http://localhost${PORT}`)
-})
-
+mongoose.connect(MONGO_URI).then(() => {console.log("Database Active"); app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)} )}
+).catch((err) => console.log(err))
